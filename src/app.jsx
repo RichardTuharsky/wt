f4a7aa49dcd683c2ad83bfaf57f6a8cb9e2bbdeb
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-04-11 20:33:06 +0200

    finally found the bug

--- src/app.jsx
+++ src/app.jsx
@@ -3,24 +3,19 @@ import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/testimonial/Testimonial'
+import Testimonial from './components/testimonial/Testimonial'
 import Contact from './components/contact/Contact'
 import Footer from  './components/footer/Footer'
 import Experience from './components/experience/Experience'
 
- const App = () => {
-  return (
-    <>
-       <Header/>
-       <Nav/>
-       <About/>
-       <Experience/>
-       <Portfolio/>
-       <Testimonials/>
-       <Contact/>
-       <Footer/>
-    </>
-  )
-}
+const App = () => (
+  <div className="app">
+    <Nav/>
+    <Header />
+    <About />
+    <Testimonial/>
+    <Footer />
+  </div>
+);
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import ReactDOM from 'react-dom'
-import app from './App'
+import App from './App'
 import './index.css'
 ReactDOM.render(<App/>,document.querySelector('#root'))
