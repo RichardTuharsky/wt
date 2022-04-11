e2b1596e1854f707940b2fdc90d7dcc0e632df12
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-04-11 20:08:24 +0200

    .

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonial/Testimonial'
+import Contact from './components/contact/Contact'
+import Footer from  './components/footer/footer'
+import Experience from './components/experience/Experience'
 
  const app = () => {
   return (
     <>
        <Header />
+       <Nav/>
+       <About/>
+       <Experience/>
+       <Portfolio/>
+       <Testimonials/>
+       <Contact/>
+       <Footer/>
     </>
   )
 }
