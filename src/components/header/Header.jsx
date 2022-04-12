920e0fbd31d2fb8ca0fd063a7f61cd732c994011
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-04-12 10:49:42 +0200

    .

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,6 +14,7 @@ function Header() {
         <div className='me'>
           <img src={ME} alt="me" />
         </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HeaderSocials() {
+  return (
+    <div className='header__socials'>
+        <a href="https://www.linkedin.com/"target="_blank"></a>
+        <a href="https://github.com/RichardTuharsky"target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
