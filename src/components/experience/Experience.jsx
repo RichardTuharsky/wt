331f08ffa16da7f2399dd47c6b6d6cd66ba0295f
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-04-15 09:10:39 +0200

    .

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -6,7 +6,9 @@ function Experience() {
     <section id='experience'>
       <h5>What Skills I have</h5>
       <h2>My experience</h2>
+      
       <div className='container experience__container'>
+        <div className='experience__frontend'>
         <h3>Frontend Development</h3>
         <div className='experience__content'>
           <article className='experience__details'>
@@ -31,8 +33,33 @@ function Experience() {
           </article>
         </div>
       </div>
-      <div className='experience__backend'>
+      </div>
+        {/*koniec frontendu*/}
 
+      <div className='experience__backend'>
+      <h3>Backend Development</h3>
+        <div className='experience__content'>
+          <article className='experience__details'>
+            <BsPatchCheckFill/>
+            <h4>Node JS</h4>
+            <small className='text-light'>Beginner</small>
+          </article>
+          <article className='experience__details'>
+            <BsPatchCheckFill/>
+            <h4>.NET CORE</h4>
+            <small className='text-light'>Beginner</small>
+          </article>
+          <article className='experience__details'>
+            <BsPatchCheckFill/>
+            <h4>C#</h4>
+            <small className='text-light'>Beginner</small>
+          </article>
+          <article className='experience__details'>
+            <BsPatchCheckFill/>
+            <h4>MySQL</h4>
+            <small className='text-light'>Beginner</small>
+          </article>
+        </div>
       </div>
       </section>
   )
