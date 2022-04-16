a9a19e9afb8cbb706925bbb0503a38a2e895deb1
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-04-16 16:49:12 +0200

    portfolio projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,46 +2,61 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.png'
 import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.png'
+import IMG4 from '../../assets/portfolio4.png'
+import IMG5 from '../../assets/portfolio5.png'
+
+/*dymaicke pridavanie projektov do portfolia*/
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Web App using React, .NET 5 and SQL **in progress**',
+    github: 'https://github.com/RichardTuharsky'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'School project interactive map in JavaScript using Google API',
+    github: 'https://github.com/RichardTuharsky/U-10--mapa'
+  },
+  {
+    id: 3,
+    image: IMG4,
+    title: 'A simple tic tac toe project that Bill Gates also started with when he was learning to code',
+    github: 'https://github.com/RichardTuharsky/tic-tac-toe'
+  },
+  {
+    id: 3,
+    image: IMG5,
+    title: 'Also one of my first projects, a drawing app',
+    github: 'https://github.com/RichardTuharsky/Drawing-app'
+  },
+] 
+
 
 function Portfolio() {
   return (
     <section id='portfolio'>
       <h5>My Recent Projects</h5>
-      <h2>Portoflio</h2>
+      <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/RichardTuharsky"className='btn'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels"className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/RichardTuharsky"className='btn'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels"className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/RichardTuharsky"className='btn'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels"className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-     
+        {
+          data.map(({id,image,title,github}) => {
+            return ( 
+              <article key={id} className='portfolio__item'>
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+              <a href={github} className='btn'>Github</a>
+              </div>
+            </article>
+            )
+          })
+        }
       </div>
- 
       </section>
   )
 }
