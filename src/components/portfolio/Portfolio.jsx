4015c8c90a1faf7bfb429197478124554f9977c1
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-04-16 13:02:01 +0200

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
+
 function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Projects</h5>
+      <h2>Portoflio</h2>
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+             <img src="{IMG1}" alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/RichardTuharsky" className='btn' target='_blank'>GitHub</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+             <img src="{IMG2}" alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/RichardTuharsky" className='btn' target='_blank'>GitHub</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        {/*<article className='portfolio__item'>
+          <div className="portfolio__item-image">
+             <img src="{IMG1}" alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/RichardTuharsky" className='btn' target='_blank'>GitHub</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+  </article> */}
+      </div>
+      </section>
   )
 }
 
