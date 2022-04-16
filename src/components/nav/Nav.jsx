611fb063b63ea70b9f64dd2d478ee08d0b234a33
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-04-16 18:14:39 +0200

    .

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -8,13 +8,13 @@ import {IoMdCall} from 'react-icons/io'
 import {useState} from 'react'
 
 function Nav() {
-  const [activeNav, setActiveNav] = useState('#')
+  const [activeNav, setActiveNav] = useState('/#')
   return (
     <nav>
       <a href="/#" onClick={() => setActiveNav('#')}className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')}className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')}className={activeNav === '#experience' ? 'active' : ''}><AiOutlineFundProjectionScreen/></a>
-      <a href="#portoflio" onClick={() => setActiveNav('#portfolio')}className={activeNav === '#portfolio' ? 'active' : ''}><BsCodeSquare/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')}className={activeNav === '#portfolio' ? 'active' : ''}><BsCodeSquare/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')}className={activeNav === '#contact' ? 'active' : ''}><IoMdCall/></a>
     </nav>
   )
