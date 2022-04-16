8f2403c9183567a476ca07588cc8ba374d06e65e
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-04-16 21:28:13 +0200

    .

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -26,8 +26,8 @@ const About = () => {
                   <small>8+</small>
                 </article>
               </div>
-              <p>
-              For the last 8 months I have been working at IBM as a software tester, I work here in an agile international team where we work on mobile and web applications for an oil conglomerate as our client, at the same time I am studying computer sciences and I also create projects as a demonstration of my mastery of the mentioned technologies and the primary goal is to make progress in software development.
+              <p className='about__text'>
+              Hello, my name is Richard, I'm glad you came to my personal portfolio page. For the last 8 months I have been working at IBM as a software tester, I work here in an agile international team where we work on mobile and web applications for an oil conglomerate as our client, at the same time I am studying computer sciences and I also create projects as a demonstration of my mastery of the mentioned technologies and the primary goal is to make progress in software development, more information you can find in my resume which can be downloaded directly here.
               </p>
               <a href="#contact" className='btn btn-primary'>Let´s Talk</a>
             </div>
